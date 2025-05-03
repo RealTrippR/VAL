7643ab6a9d572c0d9b9e1da3d58cdc9f461e05af
Tripp R <60992655+RealTrippR@users.noreply.github.com>
2025-05-03 00:48:05 -0400

    updated base test

--- TESTS/base.cpp
+++ TESTS/base.cpp
@@ -173,7 +173,7 @@ int main()
 	renderTarget.setClearValues({ { 0.0f, 0.0f, 0.0f, 1.0f } });
 	// Note that simply setting the index and vertex buffers does not update them in current command buffer, they have to be binded using rt.updateBuffers() or rt.update()
 	renderTarget.setIndexBuffer(indexBuffer, indices.size());
-	renderTarget.setVertexBuffers({ vertexBuffer }, vertices.size());
+	renderTarget.setVertexBuffer(vertexBuffer, vertices.size());
 
 	// config viewport, covers the entire size of the window
 	VkViewport viewport{ 0,0, window.getSize().width, window.getSize().height, 0.f, 1.f };
