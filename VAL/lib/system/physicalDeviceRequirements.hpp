e007f312282300406514ca19bbbdb9fd1abecfa5
Tripp R <60992655+RealTrippR@users.noreply.github.com>
2025-06-03 21:35:20 -0400

    modified physical device requirements to use tiny_vector instead

--- VAL/lib/system/physicalDeviceRequirements.hpp
+++ VAL/lib/system/physicalDeviceRequirements.hpp
@@ -20,9 +20,8 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR TH
 #define VAL_DEVICE_REQUIREMENTS_HPP
 
 #include <stdint.h>
-#include <vector>
 #include <optional>
-
+#include <VAL/lib/ext/tiny_vector.hpp>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -89,7 +88,7 @@ namespace val {
 		physicalDevicePriorities priorities{};
 		std::optional<uint32_t> deviceID;
 		std::optional<uint32_t> vendorID;
-		std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+		tiny_vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 	};
 }
 
